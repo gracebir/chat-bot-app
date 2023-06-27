6f1835174a0947d349a441844e640243435c0f6a
gracebir <grace.devolop@gmail.com>
2023-06-27 11:41:16 +0200

    add header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Header from './components/Header'
 
 function App() {
   return (
-    <div>
-      <div className="text-3xl font-bold text-gray-700">Chat bot application</div>
+    <div className='bg-gray-950 h-screen w-full'>
+      <main className='max-w-3xl bg-white mx-auto font-inter h-screen relative overflow-y-scroll scroll-hidden'>
+        <Header/>
+        
+       </main>
     </div>
   )
 }
