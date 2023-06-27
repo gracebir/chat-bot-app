ba92a0d3ef5f21bed61b5d5a512c14c92394d9c5
gracebir <grace.devolop@gmail.com>
2023-06-27 09:47:51 +0200

    getstarted

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <div className="text-3xl font-bold text-gray-700">Chat bot application</div>
+    </div>
+  )
+}
+
+export default App
